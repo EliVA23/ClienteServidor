d90497fb94df5ac2849c5da5b7d84998a5cce548
danicorralesg <dani1399cg@gmail.com>
2024-11-16 18:20:29 -0600

    Update PracticaProgramadaCS2.java

--- PracticaProgramadaCS2.java
+++ PracticaProgramadaCS2.java
@@ -1,47 +1,35 @@
-import java.util.Random;
+package org.example;
 
-/**
- *
- * @author Grupo1
- */
-
-public class PracticaProgramadaCS2 {
+public class Main {
 
     public static void main(String[] args) {
-        Random random = new Random();
         int pista = 70;
-        int posicionLiebre = 1;
-        int posicionTortuga =1;
-        
-        Tortuga tortuga = new Tortuga(pista);
-        tortuga.posicion = posicionTortuga;
-        
-        Liebre liebre = new Liebre(pista);
-        liebre.posicion = posicionLiebre;
-        
-        Thread hiloTortuga = new Thread(tortuga);
-        Thread hiloLiebre = new Thread(liebre);
 
-        hiloTortuga.start();
-        hiloLiebre.start();
+        // crear hilos
+        Tortuga tortuga = new Tortuga(pista);
+        Liebre liebre = new Liebre(pista);
 
         System.out.println("Comienza la carrera de la liebre y la tortuga!");
-        while (posicionLiebre < pista && posicionTortuga < pista) {
+
+        // iniciar hilos
+        tortuga.start();
+        liebre.start();
+
+        // monitorizar la carrera
+        while (tortuga.getPosicion() < pista && liebre.getPosicion() < pista) {
+            imprimirPista(tortuga.getPosicion(), liebre.getPosicion(), pista);
+
             try {
                 Thread.sleep(1000); // Esperar 1 segundo
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            // Movimiento de la tortuga
-            int movimientoTortuga = tortuga.calcularMovimientoTortuga(random);
-            posicionTortuga = Math.max(1, posicionTortuga + movimientoTortuga);
-            // Movimiento de la liebre
-            int movimientoLiebre  = liebre.calcularMovimientoLiebre(random);
-            posicionLiebre = Math.max(1, posicionLiebre + movimientoLiebre);
-            // Imprimir la pista
-            imprimirPista(posicionTortuga, posicionLiebre, pista);
         }
-        if (posicionTortuga == pista) {
+
+        // determinar el ganador
+        if (tortuga.getPosicion() >= pista && liebre.getPosicion() >= pista) {
+            System.out.println("Es un empate!");
+        } else if (tortuga.getPosicion() >= pista) {
             System.out.println("La tortuga ha ganado la carrera!");
         } else {
             System.out.println("La liebre ha ganado la carrera!");
@@ -50,7 +38,10 @@ public class PracticaProgramadaCS2 {
 
     private static void imprimirPista(int posicionTortuga, int posicionLiebre, int pista) {
         for (int i = 1; i <= pista; i++) {
-            if (i == posicionTortuga) {
+            if (i == posicionTortuga && i == posicionLiebre) {
+                System.out.print("TL"); // si estan en la misma casilla
+                i++; // aaltar un espacio adicional
+            } else if (i == posicionTortuga) {
                 System.out.print("T");
             } else if (i == posicionLiebre) {
                 System.out.print("L");
